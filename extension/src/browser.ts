15ebabf2ff2443eb0584ebd6f7516077c2f7b106
Shedrach Okonofua <shedrachokonofua@gmail.com>
2023-06-04 20:59:37 -0400

    Fix extension page content lag

--- extension/src/browser.ts
+++ extension/src/browser.ts
@@ -8,8 +8,11 @@ export const getTabContent = async (tab: chrome.tabs.Tab) => {
 };
 
 export const onTabLoaded = (callback: (tab: chrome.tabs.Tab) => void) => {
-  chrome.tabs.onUpdated.addListener((_, changeInfo, tab) => {
+  chrome.tabs.onUpdated.addListener(async (_, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
+      // Wait for the page to be fully loaded
+      await new Promise((resolve) => setTimeout(resolve, 750));
+
       callback(tab);
     }
   });
