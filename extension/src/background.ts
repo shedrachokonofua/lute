e04e5115ad3918fc1b907306b98ad0c57d143d43
Shedrach Okonofua <shedrachokonofua@gmail.com>
2023-08-06 10:02:48 -0400

    Remove  staleness check from extension

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,19 +1,7 @@
 import "./polyfill";
-import { isFileStale, putFile } from "./core";
+import { putFile } from "./core";
 import { onRymPageLoaded } from "./rym";
 
 (async () => {
-  onRymPageLoaded(async (fileName, content) => {
-    try {
-      if (!(await isFileStale(fileName))) {
-        console.log(`File ${fileName} is not stale`);
-        return;
-      }
-    } catch (e) {
-      console.error(`Error checking if file ${fileName} is stale`, e);
-      return;
-    }
-
-    await putFile(fileName, content);
-  });
+  onRymPageLoaded(putFile);
 })();
--- extension/src/core.ts
+++ extension/src/core.ts
@@ -3,14 +3,6 @@ import { IsFileStaleRequest, PutFileRequest } from "./proto/lute_pb";
 
 const fileService = new FileServiceClient("http://localhost:22000");
 
-export const isFileStale = async (fileName: string) =>
-  (
-    await fileService.isFileStale(
-      new IsFileStaleRequest().setName(fileName),
-      {}
-    )
-  ).getStale();
-
 export const putFile = async (fileName: string, content: string) => {
   const putFileRequest = new PutFileRequest();
   putFileRequest.setName(fileName);
