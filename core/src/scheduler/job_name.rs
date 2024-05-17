fd748098ece473877054e4fda3871c692139e5a4
Shedrach Okonofua <shedrachokonofua@gmail.com>
2024-05-17 11:25:04 -0400

    Remove HelloWorld jobname

--- core/src/scheduler/job_name.rs
+++ core/src/scheduler/job_name.rs
@@ -4,7 +4,6 @@ use strum_macros;
 #[derive(Debug, Eq, PartialEq, Clone, Hash, strum_macros::Display, EnumString)]
 #[strum(serialize_all = "snake_case")]
 pub enum JobName {
-  HelloWorld,
   ResetCrawlerRequestWindow,
   CrawlNewAlbums,
   ChangeEventSubscriberStatus,
