2bf003eda575777fafb86e1913c5e27cce017829
Shedrach Okonofua <shedrachokonofua@gmail.com>
2024-05-19 17:58:09 -0400

    Fix crawl_similar_albums logic

--- core/src/recommendations/recommendation_event_subscribers.rs
+++ core/src/recommendations/recommendation_event_subscribers.rs
@@ -24,31 +24,59 @@ async fn crawl_similar_albums(
 ) -> Result<()> {
   if let Event::ProfileAlbumAdded { file_name, .. } = event_data.payload.event {
     let album = app_context.album_repository.get(&file_name).await?;
-    if let Some(release_date) = album.release_date {
-      let file_name_string = file_name.to_string();
-      let release_type = file_name_string.split('/').collect::<Vec<&str>>()[1];
+    let file_name_string = file_name.to_string();
+    let release_type = file_name_string.split('/').collect::<Vec<&str>>()[1];
 
-      // Artists
-      for artist in album.artists {
-        if let Err(e) = app_context
-          .crawler
-          .crawler_interactor
-          .enqueue_if_stale(QueuePushParameters {
-            file_name: artist.file_name,
-            correlation_id: Some(format!("crawl_similar_albums:{}", file_name.to_string())),
-            priority: Some(Priority::Low),
-            deduplication_key: None,
-            metadata: None,
-          })
-          .await
-        {
-          warn!(
-            error = e.to_string(),
-            "Failed to enqueue artists for similar albums"
-          );
-        }
+    // Artists
+    for artist in album.artists {
+      if let Err(e) = app_context
+        .crawler
+        .crawler_interactor
+        .enqueue_if_stale(QueuePushParameters {
+          file_name: artist.file_name,
+          correlation_id: Some(format!("crawl_similar_albums:{}", file_name.to_string())),
+          priority: Some(Priority::Low),
+          deduplication_key: None,
+          metadata: None,
+        })
+        .await
+      {
+        warn!(
+          error = e.to_string(),
+          "Failed to enqueue artists for similar albums"
+        );
       }
+    }
 
+    // Same genres, same descriptors
+    if let Err(e) = app_context
+      .crawler
+      .crawler_interactor
+      .enqueue_if_stale(QueuePushParameters {
+        file_name: ChartParameters {
+          release_type: release_type.to_string(),
+          page_number: 1,
+          years_range_start: 1900,
+          years_range_end: Local::now().year() as u32,
+          include_primary_genres: Some(album.primary_genres.clone()),
+          include_descriptors: Some(album.descriptors.clone()),
+          ..Default::default()
+        }
+        .try_into()?,
+        correlation_id: Some(format!("crawl_similar_albums:{}", file_name.to_string())),
+        priority: Some(Priority::Low),
+        deduplication_key: None,
+        metadata: None,
+      })
+      .await
+    {
+      warn!(
+        error = e.to_string(),
+        "Failed to enqueue similar albums chart"
+      );
+    }
+
+    if let Some(release_date) = album.release_date {
       // Same genres, same year
       let mut primary_genres = album.primary_genres.clone();
       primary_genres.insert(0, "all".to_string());
@@ -62,34 +90,6 @@ async fn crawl_similar_albums(
             years_range_start: release_date.year() as u32,
             years_range_end: release_date.year() as u32,
             include_primary_genres: Some(primary_genres),
-            ..Default::default()
-          }
-          .try_into()?,
-          correlation_id: Some(format!("crawl_similar_albums:{}", file_name.to_string())),
-          priority: Some(Priority::Low),
-          deduplication_key: None,
-          metadata: None,
-        })
-        .await
-      {
-        warn!(
-          error = e.to_string(),
-          "Failed to enqueue similar albums chart"
-        );
-      }
-
-      // Same genres, same descriptors
-      if let Err(e) = app_context
-        .crawler
-        .crawler_interactor
-        .enqueue_if_stale(QueuePushParameters {
-          file_name: ChartParameters {
-            release_type: release_type.to_string(),
-            page_number: 1,
-            years_range_start: 1900,
-            years_range_end: Local::now().year() as u32,
-            include_primary_genres: Some(album.primary_genres.clone()),
-            include_descriptors: Some(album.descriptors.clone()),
             ..Default::default()
           }
           .try_into()?,
@@ -192,7 +192,7 @@ pub fn build_recommendation_event_subscribers(
     EventSubscriberBuilder::default()
       .id("crawl_similar_albums".to_string())
       .stream(Stream::Profile)
-      .batch_size(250)
+      .concurrency(250)
       .app_context(Arc::clone(&app_context))
       .handle(Arc::new(move |(event_data, app_context, _)| {
         Box::pin(crawl_similar_albums(event_data, app_context))
@@ -201,7 +201,7 @@ pub fn build_recommendation_event_subscribers(
     EventSubscriberBuilder::default()
       .id("save_album_spotify_tracks".to_string())
       .stream(Stream::Parser)
-      .batch_size(1)
+      .concurrency(1)
       .app_context(Arc::clone(&app_context))
       .handle(Arc::new(
         move |(event_data, app_context, subscriber_interactor)| {
