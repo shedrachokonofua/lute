9938cd728caccddbaf2950c5b2f6ec1b9e7bc395
Shedrach Okonofua <shedrachokonofua@gmail.com>
2024-05-23 18:11:28 -0400

    Log spotify album search results

--- core/src/spotify/spotify_client.rs
+++ core/src/spotify/spotify_client.rs
@@ -509,8 +509,14 @@ impl SpotifyClient {
         .unwrap_or("".to_string()),
       album.name.clone()
     );
-    match self.search(query).await? {
+    match self.search(query.clone()).await? {
       SearchResult::Albums(page) => {
+        info!(
+          file_name = album.file_name.to_string(),
+          query,
+          results = format!("{:?}", page),
+          "Spotify album search results"
+        );
         let mut candidates = vec![];
         for item in page.items.into_iter() {
           let name_similarity = jaro_winkler(
