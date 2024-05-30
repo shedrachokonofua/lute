7753e2fe13bba060be4702b97c6eb2603b9d8fe2
Shedrach Okonofua <shedrachokonofua@gmail.com>
2024-05-29 21:55:51 -0400

    Support more chat tools

--- connector/discord/src/index.ts
+++ connector/discord/src/index.ts
@@ -1,6 +1,11 @@
 import { Client, Message } from "discord.js";
 import { config } from "./config";
-import { getAlbumMonitor, recommendAlbums } from "./lute";
+import {
+  findSimilarAlbums,
+  getAlbumMonitor,
+  recommendAlbums,
+  searchAlbums,
+} from "./lute";
 import OpenAI from "openai";
 import { RunnableToolFunction } from "openai/lib/RunnableFunction";
 import {
@@ -30,8 +35,22 @@ client.login(config.discord.token);
 const startingPrompt: ChatCompletionSystemMessageParam = {
   role: "system",
   content:
-    "You are a chat front-end for a state of the art music recommendation engine. Prompt the user a bit but creatively to drill down into their preferences, without being too direct and explicits. Don't interrogate, Be engaging and succinct. Don't call functions until you are done gathering information. Call the complete function when you detect the user is done.",
+    "You are a chat front-end for a state-of-the-art music recommendation engine called Lute. Creatively drill down into the user's preferences, without being too direct and explicit. Don't interrogate, Be engaging and brief. You're here to help them discover new music, be suggestive but not imposing. When telling them about an album, be engaging and brief, don't include cover images as they maybe dead. Tell them about your capabilities to start. Extrapolate what they mean, enrich requests as you see fit for best results. Responses must be less than 2000 characters. Steer away from your own knowledge of music for recommendations, be a conduit for Lute, only use knowledge to help user prompt. Liberally call fns for context. Suggest fns that could exist with a 'dev:' in front of it.",
 };
+
+function chunkText(text: string, maxChunkLength: number): string[] {
+  const chunks = [];
+  let chunk = "";
+  for (const word of text.split(" ")) {
+    if (chunk.length + word.length > maxChunkLength) {
+      chunks.push(chunk);
+      chunk = "";
+    }
+    chunk += word + " ";
+  }
+  chunks.push(chunk);
+  return chunks;
+}
 class ChatSession {
   messages: ChatCompletionMessageParam[] = [startingPrompt];
   private genres: string[];
@@ -60,6 +79,10 @@ class ChatSession {
   }
 
   public async handleMessage(message: Message<boolean>) {
+    if (message.content.match(/<@&?\d+>/) || message.content === "reset") {
+      this.reset();
+      return;
+    }
     this.addUserMessage(message.content.replace(/<@&?\d+>/g, "").trim());
     const runner = await openai.beta.chat.completions
       .runTools({
@@ -69,52 +92,35 @@ class ChatSession {
             type: "function",
             function: {
               name: "recommendAlbums",
-              description: "Recommend albums based on user specifications.",
+              description:
+                "Recommend albums based on user specifications. Only use this to recommend albums, not to search for albums. Use searchAlbum for that. Have a specific intent in mind when using this.",
               parameters: {
                 type: "object",
                 properties: {
                   includePrimaryGenres: {
                     description: "Primary genres to include in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.genres,
-                    },
+                    $ref: "#/definitions/genreArray",
                   },
                   includeSecondaryGenres: {
                     description:
                       "Secondary genres to include in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.genres,
-                    },
+                    $ref: "#/definitions/genreArray",
                   },
                   includeLanguages: {
                     description: "Languages to include in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.languages,
-                    },
+                    $ref: "#/definitions/languageArray",
                   },
                   excludeLanguages: {
                     description: "Languages to exclude in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.languages,
-                    },
+                    $ref: "#/definitions/languageArray",
                   },
                   includeDescriptors: {
                     description: "Descriptors to include in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.descriptors,
-                    },
+                    $ref: "#/definitions/descriptorArray",
                   },
                   excludeDescriptors: {
                     description: "Descriptors to exclude in recommendations",
-                    type: "array",
-                    items: {
-                      enum: this.descriptors,
-                    },
+                    $ref: "#/definitions/descriptorArray",
                   },
                   minReleaseYear: {
                     type: "number",
@@ -123,6 +129,26 @@ class ChatSession {
                   maxReleaseYear: {
                     type: "number",
                     description: "Maximum release year for recommendations",
+                  },
+                },
+                definitions: {
+                  genreArray: {
+                    type: "array",
+                    items: {
+                      enum: this.genres,
+                    },
+                  },
+                  languageArray: {
+                    type: "array",
+                    items: {
+                      enum: this.languages,
+                    },
+                  },
+                  descriptorArray: {
+                    type: "array",
+                    items: {
+                      enum: this.descriptors,
+                    },
                   },
                 },
               },
@@ -135,24 +161,188 @@ class ChatSession {
               parse: JSON.parse,
             },
           } as RunnableToolFunction<any>,
+          {
+            type: "function",
+            function: {
+              name: "searchAlbum",
+              description:
+                "Search album based on user query. You can use this to search up for a specific album or artist's albums, but don't rely on this for recommendations, but use this to build more context. Don't use this to determine good/bad. You can also use this to get the fileName of the album to use in other functions.",
+              parameters: {
+                type: "object",
+                properties: {
+                  pagination: {
+                    type: "object",
+                    properties: {
+                      offset: {
+                        type: "number",
+                        description: "Offset for search results",
+                      },
+                      limit: {
+                        type: "number",
+                        description:
+                          "Number of results to return. Be liberal with this for artist you think have a lot of albums.",
+                      },
+                    },
+                  },
+                  query: {
+                    type: "object",
+                    properties: {
+                      text: {
+                        type: "string",
+                        description:
+                          "Text to search for. Can ONLY be an album name or artist name. Don't put genres, languages or descriptors here, it will be incorrect.",
+                      },
+                      excludeFileNames: {
+                        type: "array",
+                        items: {
+                          type: "string",
+                        },
+                        description:
+                          "File names to exclude from search results.",
+                      },
+                      includePrimaryGenres: {
+                        description:
+                          "Primary genres to include in recommendations",
+                        $ref: "#/definitions/genreArray",
+                      },
+                      includeSecondaryGenres: {
+                        description:
+                          "Secondary genres to include in recommendations",
+                        $ref: "#/definitions/genreArray",
+                      },
+                      includeLanguages: {
+                        description: "Languages to include in recommendations",
+                        $ref: "#/definitions/languageArray",
+                      },
+                      excludeLanguages: {
+                        description: "Languages to exclude in recommendations",
+                        $ref: "#/definitions/languageArray",
+                      },
+                      includeDescriptors: {
+                        description:
+                          "Descriptors to include in recommendations",
+                        $ref: "#/definitions/descriptorArray",
+                      },
+                      excludeDescriptors: {
+                        description:
+                          "Descriptors to exclude in recommendations",
+                        $ref: "#/definitions/descriptorArray",
+                      },
+                      minReleaseYear: {
+                        type: "number",
+                        description: "Minimum release year for recommendations",
+                      },
+                      maxReleaseYear: {
+                        type: "number",
+                        description: "Maximum release year for recommendations",
+                      },
+                    },
+                  },
+                },
+                required: ["query.text"],
+              },
+              function: async (args: any) => searchAlbums(args),
+              parse: JSON.parse,
+            },
+          } as RunnableToolFunction<any>,
+          {
+            type: "function",
+            function: {
+              name: "findSimilarAlbums",
+              description: "Find similar albums based on a given album.",
+              parameters: {
+                type: "object",
+                properties: {
+                  fileName: {
+                    type: "string",
+                    description:
+                      "Lute album fileName to find similar albums for. This is the primary key for an album, so don't use this to search for an album you don't know the fileName for.",
+                  },
+                  limit: {
+                    type: "number",
+                    description: "Number of results to return.",
+                  },
+                  filters: {
+                    type: "object",
+                    properties: {
+                      text: {
+                        type: "string",
+                        description:
+                          "Text to search for. Can be an album name or artist name.",
+                      },
+                      excludeFileNames: {
+                        type: "array",
+                        items: {
+                          type: "string",
+                        },
+                        description:
+                          "File names to exclude from search results.",
+                      },
+                      includePrimaryGenres: {
+                        description:
+                          "Primary genres to include in recommendations",
+                        $ref: "#/definitions/genreArray",
+                      },
+                      includeSecondaryGenres: {
+                        description:
+                          "Secondary genres to include in recommendations",
+                        $ref: "#/definitions/genreArray",
+                      },
+                      includeLanguages: {
+                        description: "Languages to include in recommendations",
+                        $ref: "#/definitions/languageArray",
+                      },
+                      excludeLanguages: {
+                        description: "Languages to exclude in recommendations",
+                        $ref: "#/definitions/languageArray",
+                      },
+                      includeDescriptors: {
+                        description:
+                          "Descriptors to include in recommendations",
+                        $ref: "#/definitions/descriptorArray",
+                      },
+                      excludeDescriptors: {
+                        description:
+                          "Descriptors to exclude in recommendations",
+                        $ref: "#/definitions/descriptorArray",
+                      },
+                      minReleaseYear: {
+                        type: "number",
+                        description: "Minimum release year for recommendations",
+                      },
+                      maxReleaseYear: {
+                        type: "number",
+                        description: "Maximum release year for recommendations",
+                      },
+                    },
+                  },
+                },
+                required: ["fileName"],
+              },
+              function: async (args: any) => findSimilarAlbums(args),
+              parse: JSON.parse,
+            },
+          } as RunnableToolFunction<any>,
         ],
         messages: this.messages,
       })
-      .on("message", (message) => console.log(message))
       .on("functionCall", (functionCall) =>
         console.log("functionCall", functionCall)
       )
       .on("functionCallResult", (functionCallResult) =>
-        console.log("functionCallResult", functionCallResult)
+        console.log("functionCallResult")
       )
-      .on("chatCompletion", (completion) => {
-        const choice = completion.choices[0];
-        this.messages.push(choice.message);
-        if (choice.message.content) {
-          message.reply(choice.message.content);
+      .on("chatCompletion", async (completion) => {
+        const content = completion.choices?.[0]?.message?.content;
+        if (content) {
+          for (const chunk of chunkText(content, 2000)) {
+            await message.reply(chunk);
+          }
         }
       });
     await runner.finalChatCompletion();
+    console.log(runner.messages);
+    this.messages = runner.messages;
   }
 
   public reset() {
@@ -161,7 +351,7 @@ class ChatSession {
 }
 
 (async () => {
-  const albumMonitor = (await getAlbumMonitor())?.monitor;
+  const albumMonitor = await getAlbumMonitor();
   let chatSession = new ChatSession(albumMonitor);
 
   client.on("messageCreate", async (message) => {
--- connector/discord/src/lute.ts
+++ connector/discord/src/lute.ts
@@ -6,7 +6,10 @@ import { google } from "./proto/google/protobuf/empty";
 export const lute = {
   albums: new core.AlbumServiceClient(
     config.coreUrl,
-    ChannelCredentials.createInsecure()
+    ChannelCredentials.createInsecure(),
+    {
+      "grpc.max_receive_message_length": 1024 * 1024 * 100,
+    }
   ),
   recommendations: new core.RecommendationServiceClient(
     config.coreUrl,
@@ -15,7 +18,7 @@ export const lute = {
 };
 
 export const getAlbumMonitor = async () => {
-  return await lute.albums.GetMonitor(new google.protobuf.Empty());
+  return (await lute.albums.GetMonitor(new google.protobuf.Empty())).monitor;
 };
 
 export interface RecommendationFilters {
@@ -48,6 +51,8 @@ export const recommendAlbums = async (settings: RecommendationSettings) => {
       excludeSecondaryGenres: settings.filters.excludeSecondaryGenres,
       includeLanguages: settings.filters.includeLanguages,
       excludeLanguages: settings.filters.excludeLanguages,
+      includeDescriptors: settings.filters.includeDescriptors,
+      excludeDescriptors: settings.filters.excludeDescriptors,
       minReleaseYear: settings.filters.minReleaseYear,
       maxReleaseYear: settings.filters.maxReleaseYear,
     }),
@@ -62,4 +67,78 @@ export const recommendAlbums = async (settings: RecommendationSettings) => {
   return recommendations.recommendations.map(
     (recommendation) => recommendation.toObject().album
   );
+};
+
+export interface Pagination {
+  offset?: number;
+  limit?: number;
+}
+
+const toPagination = (pagination?: Pagination) => {
+  return new core.SearchPagination({
+    offset: pagination?.offset,
+    limit: pagination?.limit,
+  });
+};
+export interface AlbumSearchParams {
+  query: AlbumQuery;
+  pagination: Pagination;
+}
+
+export interface AlbumQuery {
+  text: string;
+  excludeFileNames: string[];
+  includePrimaryGenres: string[];
+  excludePrimaryGenres: string[];
+  includeSecondaryGenres: string[];
+  excludeSecondaryGenres: string[];
+  includeLanguages: string[];
+  excludeLanguages: string[];
+  includeDescriptors: string[];
+  excludeDescriptors: string[];
+  minReleaseYear?: number;
+  maxReleaseYear?: number;
+}
+
+const toAlbumSearchQuery = (query?: AlbumQuery) => {
+  return new core.AlbumSearchQuery({
+    text: query?.text,
+    excludeFileNames: query?.excludeFileNames,
+    includePrimaryGenres: query?.includePrimaryGenres,
+    excludePrimaryGenres: query?.excludePrimaryGenres,
+    includeSecondaryGenres: query?.includeSecondaryGenres,
+    excludeSecondaryGenres: query?.excludeSecondaryGenres,
+    includeLanguages: query?.includeLanguages,
+    excludeLanguages: query?.excludeLanguages,
+    includeDescriptors: query?.includeDescriptors,
+    excludeDescriptors: query?.excludeDescriptors,
+    minReleaseYear: query?.minReleaseYear,
+    maxReleaseYear: query?.maxReleaseYear,
+  });
+};
+
+export const searchAlbums = async (params: AlbumSearchParams) => {
+  const request = new core.SearchAlbumsRequest({
+    query: toAlbumSearchQuery(params.query),
+    pagination: toPagination(params.pagination),
+  });
+  const results = await lute.albums.SearchAlbums(request);
+  return results.albums.map((album) => album.toObject());
+};
+
+export interface FindSimilarAlbumsParams {
+  fileName: string;
+  filters: AlbumQuery;
+  limit: number;
+}
+
+export const findSimilarAlbums = async (params: FindSimilarAlbumsParams) => {
+  const request = new core.FindSimilarAlbumsRequest({
+    fileName: params.fileName,
+    filters: toAlbumSearchQuery(params.filters),
+    embeddingKey: "voyageai-default",
+    limit: params.limit,
+  });
+  const results = await lute.albums.FindSimilarAlbums(request);
+  return results.albums.map((album) => album.toObject());
 };
